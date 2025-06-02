9bcaae889bdf9d7263c02b5e6f6d62a0e961d3a4
bhumiSundrani <bhumisundrani14@gmail.com>
2025-06-02 16:12:15 +0530

    manage auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,18 +1,25 @@
-import { NextRequest, NextResponse } from "next/server";
+import { verifyToken } from "@/lib/jwtTokenManagement"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function authMiddleware(request: NextRequest) {
-  const token = request.cookies.get('token')?.value;
-  const pathname = request.nextUrl.pathname;
+  const token = request.cookies.get('token')?.value
+  const pathname = request.nextUrl.pathname
 
   if (!token) {
-    // Allow access to /verify-user without token
     if (pathname.startsWith('/verify-user')) {
-      return NextResponse.next();
+      return NextResponse.next()
     }
-    // Redirect to /verify-user if token is missing
-    return NextResponse.redirect(new URL('/verify-user', request.url));
+    return NextResponse.redirect(new URL('/verify-user', request.url))
   }
 
-  // Token is present, allow access (without verification)
-  return NextResponse.next();
+  try {
+    const user = await verifyToken(token)
+    console.log(user)
+    return NextResponse.next()
+  } catch (error) {
+    if (pathname.startsWith('/verify-user')) {
+      return NextResponse.next()
+    }
+    return NextResponse.redirect(new URL('/verify-user', request.url))
+  }
 }
