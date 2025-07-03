57698fa43eaf7073e8624cec5b4b30a93ddf1b8b
bhumiSundrani <bhumisundrani14@gmail.com>
2025-07-03 22:50:00 +0530

    error solved

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -3,6 +3,7 @@ import ProductModel from "@/models/Product.model";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
+  req: NextRequest,
   { params }: { params: Promise<{ id: string }> }
 ) {
   await dbConnect();
