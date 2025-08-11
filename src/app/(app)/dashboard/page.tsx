9be907eb9baeae22250713531a0b72576adb44f1
bhumiSundrani <bhumisundrani14@gmail.com>
2025-08-11 22:29:33 +0530

    analytics page done

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { useRouter } from 'next/navigation'
 import React from 'react'
 import StatsCards from './StatsCards'
 import Image from 'next/image'
@@ -9,7 +8,6 @@ import TopProducts from './TopProducts'
 import LowStockAlert from './LowStockAlert'
 
 const Dashboard = () => {
-    const router = useRouter()
   return (
     <div className="min-h-screen bg-[#f5f7fa] px-2 py-4 sm:px-6 lg:px-12 overflow-auto">
         <div className="mb-3 sm:mb-6 ml-2 flex flex-col sm:flex-row justify-between sm:w-full space-y-3.5 my-auto">
